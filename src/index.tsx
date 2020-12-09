2b8ee2949dbd82f7b65f43a17be7a48539553454
George <klassnoyparen@gmail.com>
2020-12-09 19:04:56 +0300

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './index.scss'
 import { render } from 'react-dom'
-import App from './assets/components/App'
+import App from './components/App'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './assets/redux/store'
+import { store } from './redux/store'
 
 render(
   <Provider store={store}>
